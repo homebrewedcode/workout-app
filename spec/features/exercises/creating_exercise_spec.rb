06a84c65785b0e939017fb3c78d33007ce1a05ff
Dale Pollitt <dale.negativezero@gmail.com>
2018-02-15 19:25:24 -0800

    create scenario for invalid exercise inputs, added Exercise model validations

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -5,15 +5,14 @@ RSpec.feature 'When a user creates an exercise' do
   before do
     @valid_user = User.create(email: 'valid@example.com', password: 'password')
     login_as(@valid_user)
-  end
 
-  scenario 'with valid inputs' do
     visit "/"
     click_link "My Lounge"
     click_link  "New Workout"
-
     expect(page).to have_link("Back")
+  end
 
+  scenario 'with valid inputs' do
     fill_in "Duration", with: 70
     fill_in "Workout Details", with: "Weight lifting"
     fill_in "Activity Date", with: "2018-01-15"
@@ -27,4 +26,48 @@ RSpec.feature 'When a user creates an exercise' do
     expect(exercise.user_id).to eq(@valid_user.id)
 
   end
+
+
+  scenario 'with Duration not entered as a number' do
+    fill_in "Duration", with: 'INVALID_INPUT'
+    fill_in "Workout Details", with: "Weight lifting"
+    fill_in "Activity Date", with: "2018-01-15"
+    click_button "Create Exercise"
+
+
+    expect(page).to have_content("Exercise has not been created")
+    expect(page).to have_content("Duration in min is not a number")
+
+  end
+
+  scenario 'with Duration not filled in' do
+    fill_in "Workout Details", with: "Weight lifting"
+    fill_in "Activity Date", with: "2018-01-15"
+    click_button "Create Exercise"
+
+
+    expect(page).to have_content("Exercise has not been created")
+    expect(page).to have_content("Duration in min is not a number")
+  end
+
+  scenario 'with Workout Details not filled in' do
+    fill_in "Duration", with: 70
+    fill_in "Activity Date", with: "2018-01-15"
+    click_button "Create Exercise"
+
+    expect(page).to have_content("Exercise has not been created")
+    expect(page).to have_content("Workout details can't be blank")
+
+  end
+
+  scenario 'with Activity Date not filled in' do
+    fill_in "Duration", with: 70
+    fill_in "Workout Details", with: "Weight lifting"
+    click_button "Create Exercise"
+
+    expect(page).to have_content("Exercise has not been created")
+    expect(page).to have_content("Activity date can't be blank")
+  end
+
+
 end
